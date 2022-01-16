3c11243ad58b6c88f4a9f126f03bd6b2f3887768
Sebastien-A-A <94881346+Sebastien-A-A@users.noreply.github.com>
2022-01-16 07:56:38 -0800

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,3 @@ app.post("/secondPage", function(req,res) {
 
 
 ////////////////////////////////////////////////////////////////////////////////
-
-app.listen(3000, function(){
-  console.log("Server is running on port 3000");
-});
