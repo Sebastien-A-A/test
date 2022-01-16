3b3101f0eac3ee5e5869c77975789fae87a3aa76
Sebastien-A-A <94881346+Sebastien-A-A@users.noreply.github.com>
2022-01-16 07:40:20 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use (express.static("public"));
+
+////////////////////////////////////////////////////////////////////////////////
+
+app.get("/", function(req , res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/secondPage", function(req,res) {
+  res.sendFile(__dirname + "/second.html");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
